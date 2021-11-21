ef0630e87cbdddd3c476818799956819233b7e50
shah-zx <shahnawazsayyed456@gmail.com>
2021-11-22 00:54:10 +0530

    Done with the differences of jsx and normal html

---
+++ .history/src/components/Hi_20211122004006.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hi() {
+    return (
+        <div>
+           <h1>Hello </h1> 
+        </div>
+    )
+}
+
+export default Hi
+<h1>Hello </h1>
---
+++ .history/src/components/Hi_20211122004016.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hi() {
+    return (
+        <div>
+           <h1>Hi</h1> 
+        </div>
+    )
+}
+
+export default Hi
